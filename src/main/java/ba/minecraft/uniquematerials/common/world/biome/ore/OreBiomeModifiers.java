90c61b8b6e3bd25d8143cc0544d4a865fd32da4a
AzureAD\AdmirTuzovic <admir.tuzovic@appimpact.ba>
2024-08-18 11:58:15 +0200

    Added missing placed features.

--- src/main/java/ba/minecraft/uniquematerials/common/world/biome/ore/OreBiomeModifiers.java
+++ src/main/java/ba/minecraft/uniquematerials/common/world/biome/ore/OreBiomeModifiers.java
@@ -21,45 +21,29 @@ public final class OreBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_RUTILE_ORE = 
     		ModBiomeModifierHelper.createResourceKey("add_rutile_ore");
 
-    public static final ResourceKey<BiomeModifier> ADD_DEEPSLATE_RUTILE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_deepslate_rutile_ore");
-
     public static final ResourceKey<BiomeModifier> ADD_BAUXITE_ORE = 
     		ModBiomeModifierHelper.createResourceKey("add_bauxite_ore");
-
-    public static final ResourceKey<BiomeModifier> ADD_DEEPSLATE_BAUXITE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_deepslate_bauxite_ore");
-
-    public static final ResourceKey<BiomeModifier> ADD_SANDSTONE_HALITE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_sandstone_halite_ore");
-
-    public static final ResourceKey<BiomeModifier> ADD_RED_SANDSTONE_HALITE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_red_sandstone_halite_ore");
 
     public static final ResourceKey<BiomeModifier> ADD_OLIVINE_ORE = 
     		ModBiomeModifierHelper.createResourceKey("add_olivine_ore");
 
-    public static final ResourceKey<BiomeModifier> ADD_DEEPSLATE_OLIVINE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_deepslate_olivine_ore");
+    public static final ResourceKey<BiomeModifier> ADD_HALITE_ORE = 
+    		ModBiomeModifierHelper.createResourceKey("add_halite_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_BLACK_DIAMOND_ORE = 
+    		ModBiomeModifierHelper.createResourceKey("add_black_diamond_ore");
 
     public static final ResourceKey<BiomeModifier> ADD_SILVER_ORE = 
     		ModBiomeModifierHelper.createResourceKey("add_silver_ore");
 
-    public static final ResourceKey<BiomeModifier> ADD_DEEPSLATE_SILVER_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_deepslate_silver_ore");
-
-    public static final ResourceKey<BiomeModifier> ADD_NETHERRACK_CITRINE_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_netherrack_citrine_ore");
-
     public static final ResourceKey<BiomeModifier> ADD_GALENA_ORE = 
     		ModBiomeModifierHelper.createResourceKey("add_galena_ore");
-    
-    public static final ResourceKey<BiomeModifier> ADD_SANDSTONE_BLACK_DIAMOND_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_sandstone_black_diamond_ore");
 
-    public static final ResourceKey<BiomeModifier> ADD_RED_SANDSTONE_BLACK_DIAMOND_ORE = 
-    		ModBiomeModifierHelper.createResourceKey("add_red_sandstone_black_diamond_ore");
+    public static final ResourceKey<BiomeModifier> ADD_NETHER_CITRINE_ORE = 
+    		ModBiomeModifierHelper.createResourceKey("add_nether_citrine_ore");
 
+    public static final ResourceKey<BiomeModifier> ADD_NETHER_AVENTURINE_ORE = 
+    		ModBiomeModifierHelper.createResourceKey("add_nether_aventurine_ore");
     
     public static void bootstrap(BootstrapContext<BiomeModifier> context) { 
     	
@@ -68,8 +52,48 @@ public final class OreBiomeModifiers {
 
         context.register(ADD_RUTILE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
         		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
-                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.RUTILE_BURIED)),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.RUTILE_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.RUTILE_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.RUTILE_BURIED)),
                 GenerationStep.Decoration.UNDERGROUND_ORES));
-    	
+
+        context.register(ADD_BAUXITE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BAUXITE_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BAUXITE_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BAUXITE_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_OLIVINE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.OLIVINE_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.OLIVINE_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.OLIVINE_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_GALENA_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.GALENA_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.GALENA_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.GALENA_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_SILVER_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.SILVER_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.SILVER_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.SILVER_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_HALITE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.HALITE_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.HALITE_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.HALITE_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_BLACK_DIAMOND_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BLACK_DIAMOND_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BLACK_DIAMOND_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.BLACK_DIAMOND_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_NETHER_AVENTURINE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_NETHER),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.AVENTURINE_NETHER_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.AVENTURINE_NETHER_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.AVENTURINE_NETHER_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_NETHER_CITRINE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        		biomesGetter.getOrThrow(BiomeTags.IS_NETHER),
+                HolderSet.direct(placedFeaturesGetter.getOrThrow(OrePlacedFeatures.CITRINE_NETHER_SMALL), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.CITRINE_NETHER_LARGE), placedFeaturesGetter.getOrThrow(OrePlacedFeatures.CITRINE_NETHER_BURIED)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
     }
 }
--- src/main/java/ba/minecraft/uniquematerials/common/world/feature/ore/OrePlacedFeatures.java
+++ src/main/java/ba/minecraft/uniquematerials/common/world/feature/ore/OrePlacedFeatures.java
@@ -172,6 +172,28 @@ public final class OrePlacedFeatures {
         Holder<ConfiguredFeature<?, ?>> silverBuriedHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.SILVER_BURIED);
         PlacementUtils.register(context, SILVER_BURIED, silverBuriedHolder, ModPlacedFeatureHelper.createCommonTrianglePlacement(4, -64, 16));
 
+	    // Halite (placement similar to iron with recommended range 55 => 64)
+	    
+        Holder<ConfiguredFeature<?, ?>> haliteSmallHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.HALITE_SMALL);
+        PlacementUtils.register(context, HALITE_SMALL, haliteSmallHolder, ModPlacedFeatureHelper.createCommonUniformPlacement(5, 55, 64));
+
+        Holder<ConfiguredFeature<?, ?>> haliteLargeHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.HALITE_LARGE);
+        PlacementUtils.register(context, HALITE_LARGE, haliteLargeHolder, ModPlacedFeatureHelper.createCommonTrianglePlacement(20, 55, 64));
+
+        Holder<ConfiguredFeature<?, ?>> haliteBuriedHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.HALITE_BURIED);
+        PlacementUtils.register(context, HALITE_BURIED, haliteBuriedHolder, ModPlacedFeatureHelper.createCommonTrianglePlacement(10, 55, 64));
+        
+	    // Black diamond (placement similar to diamond with recommended range 55 => 64)
+	    
+        Holder<ConfiguredFeature<?, ?>> blackDiamondSmallHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.BLACK_DIAMOND_SMALL);
+        PlacementUtils.register(context, BLACK_DIAMOND_SMALL, blackDiamondSmallHolder, ModPlacedFeatureHelper.createCommonUniformPlacement(2, 0, 32));
+
+        Holder<ConfiguredFeature<?, ?>> blackDiamondLargeHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.BLACK_DIAMOND_LARGE);
+        PlacementUtils.register(context, BLACK_DIAMOND_LARGE, blackDiamondLargeHolder, ModPlacedFeatureHelper.createRareTrianglePlacement(8, 0, 32));
+
+        Holder<ConfiguredFeature<?, ?>> blackDiamondBuriedHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.BLACK_DIAMOND_BURIED);
+        PlacementUtils.register(context, BLACK_DIAMOND_BURIED, blackDiamondBuriedHolder, ModPlacedFeatureHelper.createCommonTrianglePlacement(4, 0, 32));
+        
 	    // Citrine (placement similar to quartz with recommended range 10 => 80)
 	    
         Holder<ConfiguredFeature<?, ?>> citrineSmallHolder = configuredFeaturesGetter.getOrThrow(OreConfiguredFeatures.CITRINE_NETHER_SMALL);
