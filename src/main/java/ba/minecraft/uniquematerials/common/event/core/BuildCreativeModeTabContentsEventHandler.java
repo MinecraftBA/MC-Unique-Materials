fc2e469e13e53eef3c727b77fe807c6e68e0fef1
AzureAD\AdmirTuzovic <admir.tuzovic@appimpact.ba>
2024-08-18 13:28:06 +0200

    Added missing creative tab entries.

--- src/main/java/ba/minecraft/uniquematerials/common/event/core/BuildCreativeModeTabContentsEventHandler.java
+++ src/main/java/ba/minecraft/uniquematerials/common/event/core/BuildCreativeModeTabContentsEventHandler.java
@@ -1,8 +1,8 @@
 package ba.minecraft.uniquematerials.common.event.core;
 
-import ba.minecraft.uniquematerials.common.blocks.OreBlocks;
-import ba.minecraft.uniquematerials.common.blocks.TreeBlocks;
 import ba.minecraft.uniquematerials.common.core.UniqueMaterialsMod;
+import ba.minecraft.uniquematerials.common.items.OreItems;
+import ba.minecraft.uniquematerials.common.items.TreeItems;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -22,84 +22,106 @@ public final class BuildCreativeModeTabContentsEventHandler {
 		if(tabKey == CreativeModeTabs.NATURAL_BLOCKS) {
 
 			// Ores spawning in normal heights instead of Stone.
-			event.accept(OreBlocks.BAUXITE_ORE);
-			event.accept(OreBlocks.GALENA_ORE);
-			event.accept(OreBlocks.OLIVINE_ORE);
-			event.accept(OreBlocks.RUTILE_ORE);
-			event.accept(OreBlocks.SILVER_ORE);
+			event.accept(OreItems.BAUXITE_ORE);
+			event.accept(OreItems.GALENA_ORE);
+			event.accept(OreItems.OLIVINE_ORE);
+			event.accept(OreItems.RUTILE_ORE);
+			event.accept(OreItems.SILVER_ORE);
 
 			// Ores spawning in deep instead of Deepslate.
-			event.accept(OreBlocks.DEEPSLATE_BAUXITE_ORE);
-			event.accept(OreBlocks.DEEPSLATE_OLIVINE_ORE);
-			event.accept(OreBlocks.DEEPSLATE_RUTILE_ORE);
-			event.accept(OreBlocks.DEEPSLATE_SILVER_ORE);
+			event.accept(OreItems.DEEPSLATE_BAUXITE_ORE);
+			event.accept(OreItems.DEEPSLATE_OLIVINE_ORE);
+			event.accept(OreItems.DEEPSLATE_RUTILE_ORE);
+			event.accept(OreItems.DEEPSLATE_SILVER_ORE);
 
 			// Ores spawning instead of Red Sand.
-			event.accept(OreBlocks.RED_SANDSTONE_BLACK_DIAMOND_ORE);
-			event.accept(OreBlocks.RED_SANDSTONE_HALITE_ORE);
+			event.accept(OreItems.RED_SANDSTONE_BLACK_DIAMOND_ORE);
+			event.accept(OreItems.RED_SANDSTONE_HALITE_ORE);
 
 			// Ores spawning instead of Sand.
-			event.accept(OreBlocks.SANDSTONE_BLACK_DIAMOND_ORE);
-			event.accept(OreBlocks.SANDSTONE_HALITE_ORE);
+			event.accept(OreItems.SANDSTONE_BLACK_DIAMOND_ORE);
+			event.accept(OreItems.SANDSTONE_HALITE_ORE);
 
 			// Ores spawning in Nether.
-			event.accept(OreBlocks.NETHERRACK_CITRINE_ORE);
+			event.accept(OreItems.NETHERRACK_AVENTURINE_ORE);
+			event.accept(OreItems.NETHERRACK_CITRINE_ORE);
 			
 			// Blocks of raw ores.
-			event.accept(OreBlocks.RAW_BAUXITE_BLOCK);
-			event.accept(OreBlocks.RAW_GALENA_BLOCK);
-			event.accept(OreBlocks.RAW_HALITE_BLOCK);
-			event.accept(OreBlocks.RAW_OLIVINE_BLOCK);
-			event.accept(OreBlocks.RAW_RUTILE_BLOCK);
-			event.accept(OreBlocks.RAW_SILVER_BLOCK);
+			event.accept(OreItems.RAW_BAUXITE_BLOCK);
+			event.accept(OreItems.RAW_GALENA_BLOCK);
+			event.accept(OreItems.RAW_HALITE_BLOCK);
+			event.accept(OreItems.RAW_OLIVINE_BLOCK);
+			event.accept(OreItems.RAW_RUTILE_BLOCK);
+			event.accept(OreItems.RAW_SILVER_BLOCK);
 
 			// Leaves.
-			event.accept(TreeBlocks.BEECH_LEAVES);
-			event.accept(TreeBlocks.MAHOGANY_LEAVES);
-			event.accept(TreeBlocks.SEQUOIA_LEAVES);
+			event.accept(TreeItems.BEECH_LEAVES);
+			event.accept(TreeItems.MAHOGANY_LEAVES);
+			event.accept(TreeItems.SEQUOIA_LEAVES);
 
 			// Saplings.
-			event.accept(TreeBlocks.BEECH_SAPLING);
-			event.accept(TreeBlocks.MAHOGANY_SAPLING);
-			event.accept(TreeBlocks.SEQUOIA_SAPLING);
+			event.accept(TreeItems.BEECH_SAPLING);
+			event.accept(TreeItems.MAHOGANY_SAPLING);
+			event.accept(TreeItems.SEQUOIA_SAPLING);
 
 			// Woods.
-			event.accept(TreeBlocks.BEECH_WOOD);
-			event.accept(TreeBlocks.MAHOGANY_WOOD);
-			event.accept(TreeBlocks.SEQUOIA_WOOD);
+			event.accept(TreeItems.BEECH_WOOD);
+			event.accept(TreeItems.MAHOGANY_WOOD);
+			event.accept(TreeItems.SEQUOIA_WOOD);
 
 			// Logs (available as both natural and building blocks)
-			event.accept(TreeBlocks.BEECH_LOG);
-			event.accept(TreeBlocks.MAHOGANY_LOG);
-			event.accept(TreeBlocks.SEQUOIA_LOG);
+			event.accept(TreeItems.BEECH_LOG);
+			event.accept(TreeItems.MAHOGANY_LOG);
+			event.accept(TreeItems.SEQUOIA_LOG);
 
 		}
 		
 		if(tabKey == CreativeModeTabs.BUILDING_BLOCKS) {
 
-			event.accept(OreBlocks.ALUMINIUM_BLOCK);
-			event.accept(OreBlocks.LEAD_BLOCK);
-			event.accept(OreBlocks.SILICON_BLOCK);
-			event.accept(OreBlocks.SILVER_BLOCK);
-			event.accept(OreBlocks.TITANIUM_BLOCK);
+			event.accept(OreItems.ALUMINIUM_BLOCK);
+			event.accept(OreItems.LEAD_BLOCK);
+			event.accept(OreItems.SILICON_BLOCK);
+			event.accept(OreItems.SILVER_BLOCK);
+			event.accept(OreItems.TITANIUM_BLOCK);
 
 			// Logs (available as both natural and building blocks)
-			event.accept(TreeBlocks.BEECH_LOG);
-			event.accept(TreeBlocks.MAHOGANY_LOG);
-			event.accept(TreeBlocks.SEQUOIA_LOG);
+			event.accept(TreeItems.BEECH_LOG);
+			event.accept(TreeItems.MAHOGANY_LOG);
+			event.accept(TreeItems.SEQUOIA_LOG);
 
 			// Stripped logs and woods.
-			event.accept(TreeBlocks.STRIPPED_BEECH_LOG);
-			event.accept(TreeBlocks.STRIPPED_BEECH_WOOD);
-			event.accept(TreeBlocks.STRIPPED_MAHOGANY_LOG);
-			event.accept(TreeBlocks.STRIPPED_MAHOGANY_WOOD);
-			event.accept(TreeBlocks.STRIPPED_SEQUOIA_LOG);
-			event.accept(TreeBlocks.STRIPPED_SEQUOIA_WOOD);
+			event.accept(TreeItems.STRIPPED_BEECH_LOG);
+			event.accept(TreeItems.STRIPPED_BEECH_WOOD);
+			event.accept(TreeItems.STRIPPED_MAHOGANY_LOG);
+			event.accept(TreeItems.STRIPPED_MAHOGANY_WOOD);
+			event.accept(TreeItems.STRIPPED_SEQUOIA_LOG);
+			event.accept(TreeItems.STRIPPED_SEQUOIA_WOOD);
 
 			// Planks.
-			event.accept(TreeBlocks.BEECH_PLANKS);
-			event.accept(TreeBlocks.MAHOGANY_PLANKS);
-			event.accept(TreeBlocks.SEQUOIA_PLANKS);
+			event.accept(TreeItems.BEECH_PLANKS);
+			event.accept(TreeItems.MAHOGANY_PLANKS);
+			event.accept(TreeItems.SEQUOIA_PLANKS);
+
+		}
+		
+		if(tabKey == CreativeModeTabs.INGREDIENTS) {
+
+			event.accept(OreItems.RAW_BAUXITE);
+			event.accept(OreItems.RAW_GALENA);
+			event.accept(OreItems.RAW_HALITE);
+			event.accept(OreItems.RAW_OLIVINE);
+			event.accept(OreItems.RAW_RUTILE);
+			event.accept(OreItems.RAW_SILVER);
+
+			event.accept(OreItems.AVENTURINE);
+			event.accept(OreItems.BLACK_DIAMOND);
+			event.accept(OreItems.CITRINE);
+
+			event.accept(OreItems.ALUMINIUM_INGOT);
+			event.accept(OreItems.LEAD_INGOT);
+			event.accept(OreItems.SILICON_INGOT);
+			event.accept(OreItems.SILVER_INGOT);
+			event.accept(OreItems.TITANIUM_INGOT);
 
 		}
 
